a22749de18ba1dcd611fdeae9e44457fc2a9eef5
CH <ch_hsu@asus.com>
2017-10-16 18:37:04 +0800

    wiring up weather search

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*
 const yargs = require('yargs');
 
 const geocode = require('./geocode/geocode');
@@ -24,3 +25,23 @@ geocode.geocodeAddress(argv.a, (errorMessage, results) => {
         console.log(JSON.stringify(results, undefined, 2));
     }
 });
+*/
+// 1a5e6c07f1fb77d34bdae5ba084e3168
+
+const request = require('request');
+
+request({
+    url: 'https://api.darksky.net/forecast/1a5e6c07f1fb77d34bdae5ba084e3168/39.9396284,-75.18663959999999',
+    json: true
+    
+}, (error, response, body) => {
+    //console.log(JSON.stringify(response, undefined, 2));
+
+    if (!error && response.statusCode === 200) {
+        console.log(`Temperature: ${body.currently.temperature}`);
+    } else {
+        console.log('Unable to fetch weather.');
+    }  
+    
+});
+

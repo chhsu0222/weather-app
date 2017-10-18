11a5154e0bb051f4f956c8616ed5c60975934957
CH <ch_hsu@asus.com>
2017-10-18 11:18:43 +0800

    example of Promises with input

--- playground/promise.js
+++ playground/promise.js
@@ -1,18 +1,32 @@
-var somePromise = new Promise((resolve, reject) => {
-    // resolve function is for fulfilled and reject is for rejected
-    // only one argument for resolve and reject
-    setTimeout(() => {
-        /*
-        You can only either resolve or reject a promise once.
-        If you resolve a promise you can't reject it later.
-        If you resolve that with one value you can't change your mind 
-        at a later point in time.
-        */
-        //resolve('Hey. It worked!');
-        reject('Unable to fulfill promise');
-    }, 2500);
-    
+var asyncAdd = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (typeof a === 'number' && typeof b === 'number') {
+                resolve(a + b);
+            } else {
+                reject('Argumenta must be numbers');
+            }
+        }, 1500);
+    });
+};
+/*
+Anyone who happens to call AsyncAdd can add a 'then' call on to the 
+return result to get that value.
+*/
+
+asyncAdd(5, 7).then((res) => {
+    console.log('Result:', res);
+}, (errorMessage) => {
+    console.log(errorMessage);
 });
+
+//var somePromise = new Promise((resolve, reject) => {
+//    setTimeout(() => {
+        //resolve('Hey. It worked!');
+//        reject('Unable to fulfill promise');
+//    }, 2500);
+    
+//});
 
 /* 
 then lets us provide callback functions for both success cases and error cases
@@ -28,8 +42,8 @@ p.then(function(value) {
 });
 
 */
-somePromise.then((message) => {
-    console.log('Sucess:', message);
-}, (errorMessage) => {
-    console.log('Error:', errorMessage);
-});
+//somePromise.then((message) => {
+//    console.log('Sucess:', message);
+//}, (errorMessage) => {
+//    console.log('Error:', errorMessage);
+//});
